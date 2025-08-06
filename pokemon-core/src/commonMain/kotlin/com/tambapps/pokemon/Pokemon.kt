3d8fc06186f2f016344b214f4c8938339a18375a
Nelson Fonkoua <tambapps@gmail.com>
2025-08-06 22:06:11 +0200

    added stat bonus malus in natures

--- pokemon-core/src/commonMain/kotlin/com/tambapps/pokemon/Pokemon.kt
+++ pokemon-core/src/commonMain/kotlin/com/tambapps/pokemon/Pokemon.kt
@@ -16,33 +16,37 @@ data class Pokemon(
   val evs: PokeStats,
 )
 
-// TODO make boosts information retrievable
-enum class Nature {
-  ADAMANT, // +Atk -SpA
-  BOLD, // +Def -Atk
-  BRAVE, // +Atk -Spe
-  CALM, // +SpD -Atk
-  CAREFUL, // +SpD -SpA
-  DOCILE, // Neutral
-  GENTLE, // +SpD -Def
-  HARDY, // Neutral
-  HASTY, // +Spe -Def
-  IMPISH, // +Def -SpA
-  JOLLY, // +Spe -SpA
-  LAX, // +Def -SpD
-  LONELY, // +Atk -Def
-  MILD, // +SpA -Def
-  MODEST, // +SpA -Atk
-  NAIVE, // +Spe -SpD
-  NAUGHTY, // +Atk -SpD
-  QUIET, // +SpA -Spe
-  QUIRKY, // Neutral
-  RASH, // +SpA -SpD
-  RELAXED, // +Def -Spe
-  SASSY, // +SpD -Spe
-  SERIOUS, // Neutral
-  TIMID, // +Spe -Atk
-  BASHFUL, // Neutral
+enum class Nature(
+  val bonusStat: Stat?,
+  val malusStat: Stat?
+) {
+  ADAMANT(bonusStat = Stat.ATTACK, malusStat = Stat.SPECIAL_ATTACK),
+  BOLD(bonusStat = Stat.DEFENSE, malusStat = Stat.ATTACK),
+  BRAVE(bonusStat = Stat.ATTACK, malusStat = Stat.SPEED),
+  CALM(bonusStat = Stat.SPECIAL_DEFENSE, malusStat = Stat.ATTACK),
+  CAREFUL(bonusStat = Stat.SPECIAL_DEFENSE, malusStat = Stat.SPECIAL_ATTACK),
+  DOCILE(bonusStat = null, malusStat = null),
+  GENTLE(bonusStat = Stat.SPECIAL_DEFENSE, malusStat = Stat.DEFENSE),
+  HARDY(bonusStat = null, malusStat = null),
+  HASTY(bonusStat = Stat.SPEED, malusStat = Stat.DEFENSE),
+  IMPISH(bonusStat = Stat.DEFENSE, malusStat = Stat.SPECIAL_ATTACK),
+  JOLLY(bonusStat = Stat.SPEED, malusStat = Stat.SPECIAL_ATTACK),
+  LAX(bonusStat = Stat.DEFENSE, malusStat = Stat.SPECIAL_DEFENSE),
+  LONELY(bonusStat = Stat.ATTACK, malusStat = Stat.DEFENSE),
+  MILD(bonusStat = Stat.SPECIAL_ATTACK, malusStat = Stat.DEFENSE),
+  MODEST(bonusStat = Stat.SPECIAL_ATTACK, malusStat = Stat.ATTACK),
+  NAIVE(bonusStat = Stat.SPEED, malusStat = Stat.SPECIAL_DEFENSE),
+  NAUGHTY(bonusStat = Stat.ATTACK, malusStat = Stat.SPECIAL_DEFENSE),
+  QUIET(bonusStat = Stat.SPECIAL_ATTACK, malusStat = Stat.SPEED),
+  QUIRKY(bonusStat = null, malusStat = null),
+  RASH(bonusStat = Stat.SPECIAL_ATTACK, malusStat = Stat.SPECIAL_DEFENSE),
+  RELAXED(bonusStat = Stat.DEFENSE, malusStat = Stat.SPEED),
+  SASSY(bonusStat = Stat.SPECIAL_DEFENSE, malusStat = Stat.SPEED),
+  SERIOUS(bonusStat = null, malusStat = null),
+  TIMID(bonusStat = Stat.SPEED, malusStat = Stat.ATTACK),
+  BASHFUL(bonusStat = null, malusStat = null);
+
+  val isNeutral: Boolean get() = bonusStat == null && malusStat == null
 }
 
 enum class Gender {
