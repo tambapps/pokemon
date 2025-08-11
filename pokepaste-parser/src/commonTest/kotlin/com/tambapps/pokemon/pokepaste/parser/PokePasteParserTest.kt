de0736e1cae63441a4f876089ee8f8dc65453b10
Nelson Fonkoua <tambapps@gmail.com>
2025-08-11 10:55:05 +0200

    renamed class

--- pokepaste-parser/src/commonTest/kotlin/com/tambapps/pokemon/pokepaste/parser/PokePasteParserTest.kt
+++ pokepaste-parser/src/commonTest/kotlin/com/tambapps/pokemon/pokepaste/parser/PokePasteParserTest.kt
@@ -11,7 +11,7 @@ import kotlin.test.assertContains
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 
-class PokepasteParserTest {
+class PokePasteParserTest {
 
   private val parser = PokepasteParser()
 
